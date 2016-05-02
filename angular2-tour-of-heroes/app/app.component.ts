ec88dad9b1bc48fe44a2fe064bbd9e85eb55a294
kuzbida <kuzbida@gmail.com>
2016-05-02 18:36:45 +0300

    changes

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,31 @@
+import {Component} from 'angular2/core';
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+};
+
+@Component({
+    selector: 'my-app',
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input value="{{hero.name}}" placeholder="name">
+      </div>
+      `
+
+
+})
+
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero = 'Windstorm';
+    hero = Hero;
 }
