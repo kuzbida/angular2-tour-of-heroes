e4dbd80615cd61083b827edab55da66095c3b69b
kuzbida <kuzbida@gmail.com>
2016-05-03 13:47:08 +0300

    'changes'

--- angular2-tour-of-heroes/app/app.component.js
+++ angular2-tour-of-heroes/app/app.component.js
@@ -1,49 +1,34 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
-    var core_1;
-    var Hero, AppComponent;
-    return {
-        setters:[
-            function (core_1_1) {
-                core_1 = core_1_1;
-            }],
-        execute: function() {
-            Hero = (function () {
-                function Hero() {
-                }
-                return Hero;
-            }());
-            exports_1("Hero", Hero);
-            hero: exports_1("Hero", Hero = {
-                id: 1,
-                name: 'Windstorm'
-            });
-            AppComponent = (function () {
-                function AppComponent() {
-                    this.title = 'Tour of Heroes';
-                    this.hero = Hero;
-                }
-                AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input value=\"{{hero.name}}\" placeholder=\"name\">\n      </div>\n      "
-                    }), 
-                    __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
-            }());
-            exports_1("AppComponent", AppComponent);
-        }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
-});
+};
+var core_1 = require('angular2/core');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
+exports.Hero = Hero;
+hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+};
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = Hero;
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><h1>id: </h1>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n      </div>"
+        })
+    ], AppComponent);
+    return AppComponent;
+})();
+exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -15,14 +15,11 @@ hero: Hero = {
     template:`
       <h1>{{title}}</h1>
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div><h1>id: </h1>{{hero.id}}</div>
       <div>
         <label>name: </label>
-        <input value="{{hero.name}}" placeholder="name">
-      </div>
-      `
-
-
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>`
 })
 
 export class AppComponent {
